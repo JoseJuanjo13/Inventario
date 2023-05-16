52548871a0a65d7f21ce3c54a849f157e8de4cfb
OrtizJohana <jecheverryo_1@uqvirtual.edu.co>
2023-05-16 00:06:39 -0500

    se agregan test

--- negocio/src/test/java/co/edu/uniquindio/inventario/test/PruebasTest.java
+++ negocio/src/test/java/co/edu/uniquindio/inventario/test/PruebasTest.java
@@ -2,8 +2,10 @@ package co.edu.uniquindio.inventario.test;
 
 import co.edu.uniquindio.inventario.entidades.Bodega;
 import co.edu.uniquindio.inventario.entidades.DetalleOrdenCompra;
+import co.edu.uniquindio.inventario.entidades.Usuario;
 import co.edu.uniquindio.inventario.repo.BodegaRepo;
 import co.edu.uniquindio.inventario.repo.DetalleOrdenCompraRepo;
+import co.edu.uniquindio.inventario.repo.UsuarioRepo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,13 +26,26 @@ public class PruebasTest {
     @Autowired
     private DetalleOrdenCompraRepo detalleOrdenCompraRepo;
 
+    @Autowired
+    private UsuarioRepo usuarioRepo;
+
     @Test
     public void registrar(){
         ArrayList<String> tel = new ArrayList<>();
         tel.add("3137708899");
         Bodega bodega = new Bodega("Bodega 1", "Bod1", "Centro", tel, "Activo");
         Bodega guardado = bodegaRepo.save(bodega);
-        Assertions.assertEquals(bodega.getNombre(), guardado.getNombre());
+        Assertions.assertEquals("Bod1",guardado.getAbreviacion());
+    }
+
+    @Test
+    public void registrarUsuario(){
+        Usuario usuario = new Usuario();
+        usuario.setApellido("cardenas"); usuario.setCedula("1094567");
+        usuario.setContrasena("123"); usuario.setEmail("correo@correo.com");
+        usuario.setNombre("mario"); usuario.setTelefono("13131");
+        Usuario guardado = usuarioRepo.save(usuario);
+        Assertions.assertEquals(usuario.getNombre(), guardado.getNombre());
     }
 
     @Test
